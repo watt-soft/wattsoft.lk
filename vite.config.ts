ae9ca534fc0f3e46cac14df84ab2fca9bc31c67d
WDK-Techno <wdilshankavindra@gmail.com>
2025-08-23 22:37:51 +0530

    changed for domain

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/wattsoft.lk/',
+  // base: '/wattsoft.lk/',
+  base: '/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
